ed98045fb491055c737135026cd43a5e0b3ca5bb
Olivier Monnier <omonnier.dev@gmail.com>
2018-04-20 17:29:16 +0200

    fix

--- src/js/plugins/mavo/commands.js
+++ src/js/plugins/mavo/commands.js
@@ -7,14 +7,18 @@ export default function (editor, config = {}) {
       const css = editor.getCss();
       const newWindow = window.open();
 
-      const script = newWindow.document.createElement('script');
-      script.src = `${window.location.origin}/mavo/mavo.min.js`;
-      newWindow.document.head.appendChild(script);
-
       const link = newWindow.document.createElement('link');
       link.href = `${window.location.origin}/mavo/mavo.css`;
       link.rel = 'stylesheet';
       newWindow.document.head.appendChild(link);
+
+      const script = newWindow.document.createElement('script');
+      script.src = `${window.location.origin}/mavo/mavo.min.js`;
+      newWindow.document.head.appendChild(script);
+
+      const styles = newWindow.document.createElement('style');
+      styles.textContent = css;
+      newWindow.document.head.appendChild(styles);
       
       newWindow.document.body.innerHTML = html;
     }
--- src/js/plugins/mavo/components.js
+++ src/js/plugins/mavo/components.js
@@ -3,6 +3,52 @@ export default function (editor, config = {}) {
   let defaultModel = DomComponents.getType('default').model;
   let defaultView = DomComponents.getType('default').view;
 
+  DomComponents.addType('app-container', {
+    model: defaultModel.extend({
+      defaults: Object.assign({}, defaultModel.prototype.defaults, {
+        traits: [
+          {
+            type: 'input',
+            label: 'Id',
+            name: 'id'
+          },
+          {
+            type: 'input',
+            label: 'Title',
+            name: 'title'
+          },
+          {
+            type: 'select',
+            label: 'Mode',
+            name: 'mv-mode',
+            default: 'read',
+            options: [
+              { name: 'read', value: 'read' },
+              { name: 'edit', value: 'edit' }
+            ]
+          },
+          {
+            type: 'select',
+            label: 'Storage',
+            name: 'mv-storage',
+            default: 'none',
+            options: [
+              { name: 'none', value: 'none' },
+              { name: 'local', value: 'local' }
+            ]
+          }
+        ]
+      })
+    }, {
+      isComponent(el) {
+        if (typeof el.hasAttribute == 'function' && el.hasAttribute('mv-app')) {
+          return { type: 'app-container' }
+        }
+      }
+    }),
+    view: defaultView
+  });
+
   DomComponents.addType('default', {
     model: defaultModel.extend({
       defaults: Object.assign({}, defaultModel.prototype.defaults, {
@@ -48,39 +94,54 @@ export default function (editor, config = {}) {
     view: defaultView
   });
 
-  DomComponents.addType('app-container', {
-    model: defaultModel.extend({
-      defaults: Object.assign({}, defaultModel.prototype.defaults, {
-        traits: [
-          {
-            type: 'input',
-            label: 'Id',
-            name: 'id'
-          },
-          {
-            type: 'input',
-            label: 'Title',
-            name: 'title'
-          },
-          {
-            type: 'select',
-            label: 'Storage',
-            name: 'mv-storage',
-            default: 'none',
-            options: [
-              { name: 'none', value: 'none' },
-              { name: 'local', value: 'local' }
-            ]
-          }
-        ]
+  defaultModel = DomComponents.getType('default').model;
+  defaultView = DomComponents.getType('default').view;
+
+  const inputModel = DomComponents.getType('input').model;
+  const inputView = DomComponents.getType('input').view;
+
+  DomComponents.addType('input', {
+    model: inputModel.extend({
+      defaults: Object.assign({}, inputModel.prototype.defaults, {
+        traits: inputModel.prototype.defaults.traits.concat(defaultModel.prototype.defaults.traits)
       })
-    }, {
-      isComponent(el) {
-        if (typeof el.hasAttribute == 'function' && el.hasAttribute('mv-app')) {
-          return { type: 'app-container' }
-        }
-      }
     }),
-    view: defaultView
-  })
+    view: inputView
+  });
+
+  const textareaModel = DomComponents.getType('textarea').model;
+  const textareaView = DomComponents.getType('textarea').view;
+
+  DomComponents.addType('textarea', {
+    model: textareaModel.extend({
+      defaults: Object.assign({}, textareaModel.prototype.defaults, {
+        traits: textareaModel.prototype.defaults.traits.concat(defaultModel.prototype.defaults.traits)
+      })
+    }),
+    view: textareaView
+  });
+
+  const selectModel = DomComponents.getType('select').model;
+  const selectView = DomComponents.getType('select').view;
+
+  DomComponents.addType('select', {
+    model: selectModel.extend({
+      defaults: Object.assign({}, selectModel.prototype.defaults, {
+        traits: selectModel.prototype.defaults.traits.concat(defaultModel.prototype.defaults.traits)
+      })
+    }),
+    view: selectView
+  });
+
+  const checkboxModel = DomComponents.getType('checkbox').model;
+  const checkboxView = DomComponents.getType('checkbox').view;
+
+  DomComponents.addType('checkbox', {
+    model: checkboxModel.extend({
+      defaults: Object.assign({}, checkboxModel.prototype.defaults, {
+        traits: checkboxModel.prototype.defaults.traits.concat(defaultModel.prototype.defaults.traits)
+      })
+    }),
+    view: checkboxView
+  });
 }
